2bc2a39d08d72ce2826f9ca1b0a9b89a6d69d2a5
Adeline Graciani <50612536+8deline@users.noreply.github.com>
2021-07-17 16:50:19 +0800

    added pages and components directory and react-router-rom

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
 import "./App.css";
+import { HashRouter, Route, Switch } from "react-router-dom";
+import Home from "./pages/Home";
+import Upload from "./pages/Upload";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <HashRouter>
+      <Switch>
+        <Route path="/upload">
+          <Upload />
+        </Route>
+        <Route path="/">
+          <Home />
+        </Route>
+      </Switch>
+    </HashRouter>
+  );
 }
 
 export default App;
