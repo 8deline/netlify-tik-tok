f7c9b6a4f13df8f90d5b67c62df055d2485c5edb
Adeline Graciani <50612536+8deline@users.noreply.github.com>
2021-07-23 09:53:50 +0800

    added posts in home.js

---
+++ src/components/FollowersColumn.js
@@ -0,0 +1,5 @@
+const FollowersColumn = () => {
+  return <></>;
+};
+
+export default FollowersColumn;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,6 @@
 import fetch from "node-fetch";
 import { useState, useEffect } from "react";
+import FollowersColumn from "../components/FollowersColumn";
 
 const Home = () => {
   let [posts, setPosts] = useState(null);
@@ -14,13 +15,22 @@ const Home = () => {
 
   return (
     <div>
-      {posts &&
-        posts.map((post) => (
-          <div key={post.id}>
-            <img src={post.avatar} alt="avatar" width="40" />
-            <p>{post.username}</p>
-          </div>
-        ))}
+      <FollowersColumn />
+      <div className="feed">
+        {posts &&
+          posts.map((post) => (
+            <div key={post.id}>
+              <img src={post.avatar} alt="avatar" width="40" />
+              <p>{post.username}</p>
+              <video width="320" height="240" controls>
+                <source src={post.video_posts} type="video/mp4" />
+              </video>
+              <p>{posts.likes}</p>
+              <p>{posts.comments}</p>
+            </div>
+          ))}
+      </div>
+      <div className="suggested-box"></div>
     </div>
   );
 };
