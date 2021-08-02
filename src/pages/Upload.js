67ddb571c3ce9595ba0837946b37225a8febce6b
Adeline Graciani <50612536+8deline@users.noreply.github.com>
2021-08-02 16:30:27 +0800

    added upload page

--- src/pages/Upload.js
+++ src/pages/Upload.js
@@ -1,5 +1,49 @@
+import { useState } from "react";
+
 const Upload = () => {
-  return <div>This is upload</div>;
+  const [caption, setCaption] = useState(null);
+  const [video, setVideo] = useState(null);
+
+  const username = "ledaine";
+
+  const handleSubmit = (e) => {};
+
+  return (
+    <div className="upload-page">
+      <br />
+      <h1>Upload video</h1>
+      <p>This video will be published to @{username}</p>
+      <div className="container">
+        <form onSubmit={handleSubmit}>
+          <div className="section">
+            <div className="image-upload"></div>
+            <div className="form-section">
+              <div className="section input-section">
+                <label className="bold">Caption</label>
+                <input
+                  className="input"
+                  name="caption"
+                  value={caption}
+                  onChange={(e) => setCaption(e.target.value)}
+                />
+              </div>
+              <div className="break"></div>
+              <div className="section input-section">
+                <label className="bold">Video Url</label>
+                <input
+                  className="input"
+                  name="video"
+                  value={video}
+                  onChange={(e) => setVideo(e.target.value)}
+                />
+              </div>
+            </div>
+          </div>
+          <button>Post</button>
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default Upload;
